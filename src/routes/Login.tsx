75f0ff3e77c99f2b3dab18868b7bf43806da880b
Jaxeetee <jacob.esclamado19@gmail.com>
2024-05-09 23:56:13 +1000

    updated Login page

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -7,8 +7,12 @@ const Login = () => {
   const [tokenInput, setTokenInput] = useState<string>();
 
 
-  const onClicked = (e: any) => {
+  const submit = (e: any) => {
     console.log(e);
+  }
+
+  const getNewToken = () => {
+
   }
 
   const onUpdateInput = (e: any) => {
@@ -16,14 +20,22 @@ const Login = () => {
   }
 
   return (
-    <div className='bg-slate-900 w-screen h-screen text-center text-stone-50 content-center'>
-      <Card className='max-w-60 mx-auto'>
+    <div className='flex bg-slate-900 text-stone-50 w-screen h-screen text-center items-center justify-center space-x-10'>
+      <Card className='max-w-60 min-h-60'>
         <CardHeader>
           <CardTitle>Login using your token</CardTitle>
         </CardHeader>
         <CardContent className='m-5'>
           <Input placeholder='insert token here' className='rounded-[5px]' onChange={onUpdateInput} />
-          <Button className='bg-slate-950 rounded-xl' type="submit" onClick={onClicked}>Login</Button>
+          <Button className='m-4 bg-slate-950 rounded-xl' type="submit" onClick={submit}>Login</Button>
+        </CardContent>
+      </Card>
+      <Card className='max-w-60 min-h-60'>
+        <CardHeader>
+          <CardTitle>Create new Token</CardTitle>
+        </CardHeader>
+        <CardContent className='m-5'>
+          <Button className='m-4 bg-slate-950 rounded-xl' type="submit" onClick={getNewToken}>Generate new token</Button>
         </CardContent>
       </Card>
     </div>
