7275dfab3acc629498d7ef9d4ddec9c3a3f674b4
Jaxeetee <jacob.esclamado19@gmail.com>
2024-05-24 23:13:19 +1000

    removed App.tsx as it is not being used anymore

---
+++ src/spacetraders-api/my/register.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import API from "../api";
+import { AccountData, MY_TOKEN } from "./types";
+import { error } from "console";
+
+ 
+export async function generateToken(callsign: string): Promise<any>
+{
+
+  const data = {
+      symbol: callsign,
+      faction: "COSMIC",
+  };
+
+  const headers = {
+    'Content-Type': 'application/json',
+  }
+
+  try{
+    const response = await API.post('/register', data, { headers })
+
+    console.log(response);
+
+    if (response.status == 409)
+    {
+      throw new Error("Invalid Callsign");
+    } else {
+      return response.data.data.token;
+    }
+    } catch (error) {
+    // TODO: if error, return error code. Will probably use toasters.
+    console.error('Error generating token:', error);
+    throw error; // Re-throw for proper handling at the call site
+  }
+}
+
